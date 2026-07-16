8613957d7f7fd398a9d8527edf015bb8feaa1df3
PFTSAFETY <hllpftsafety@gmail.com>
2026-07-16 11:59:52 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // Run from the same folder as index.html (GitHub Pages / any static host).
 // ========================
 
-const CACHE_NAME = 'aitr-shell-v9';
+const CACHE_NAME = 'aitr-shell-v10';
 const SHELL_ASSETS = ['./', './index.html'];
 
 self.addEventListener('install', (event) => {
